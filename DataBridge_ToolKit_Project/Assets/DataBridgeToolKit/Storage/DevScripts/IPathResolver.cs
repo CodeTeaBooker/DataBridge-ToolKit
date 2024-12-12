bec84adcb9344fe9a66bc3a7cfa5673fbf346c3d
CodeTeaBooker <66515793+CodeTeaBooker@users.noreply.github.com>
2024-12-12 22:41:22 +0100

    chore: remove unused code and folders

--- DataBridge_ToolKit_Project/Assets/DataBridgeToolKit/Storage/DevScripts/IPathResolver.cs
+++
@@ -1,9 +0,0 @@
-using DataBridgeToolKit.Storage.Utils;
-
-namespace DataBridgeToolKit.Storage.Core.Interfaces
-{
-    public interface IPathResolver
-    {
-        StoragePath ResolvePath(string key, string extension);
-    }
-}
--- DataBridge_ToolKit_Project/Assets/DataBridgeToolKit/Storage/DevScripts/ITempFileGenerator.cs
+++
@@ -1,7 +0,0 @@
-namespace DataBridgeToolKit.Storage.Core.Interfaces
-{
-    public interface ITempFileGenerator
-    {
-        string GenerateSecureTempPath(string originalPath);
-    }
-}
